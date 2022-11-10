e281d470e09602fb06fdbdc67f8823ef8d834469
MauricioAires <mauricioaires015@gmail.com>
2022-11-10 00:43:59 -0300

    feat: componentizar o header

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,19 @@
+import { Avatar, Box, Flex, Text } from '@chakra-ui/react'
+
+export function Profile() {
+  return (
+    <Flex align="center">
+      <Box mr="4" textAlign="right">
+        <Text>Mauricio Aires</Text>
+        <Text color="gray.300" fontSize="small">
+          mauricioaires015@gmail.com
+        </Text>
+      </Box>
+      <Avatar
+        size="md"
+        name="Mauricio Aires"
+        src="https://github.com/MauricioAires.png"
+      />
+    </Flex>
+  )
+}
