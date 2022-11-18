88d4138dc3661617f974963573b7eb6372925681
MauricioAires <mauricioaires015@gmail.com>
2022-11-18 00:24:40 -0300

    feat: configurar react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,19 +5,26 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { makeServer } from '../services/mirage'
 
 import { theme } from '../styles/theme'
+import { QueryClientProvider, QueryClient } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
 
 if (process.env.NODE_ENV === 'development') {
   makeServer()
 }
 
+const queryClient = new QueryClient()
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
 
